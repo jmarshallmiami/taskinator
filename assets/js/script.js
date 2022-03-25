07ca86321720c76e96076db4b650d4e8c96501ee
Jules <j.marshallmiami@yahoo.com>
2022-03-25 13:34:27 -0500

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var formEl = document.querySelector("#task-form");
 // container elements for to-do, in-progress, and completed tasks
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
-var tasksCompleted = document.querySelector("#tasks-completed")
+var tasksCompletedEl = document.querySelector("#tasks-completed")
 // parent element to all three task categories, main
 var pageContentEl = document.querySelector("#page-content");
 
@@ -37,12 +37,11 @@ var taskFormHandler = function (event) {
             name: taskNameInput,
             type: taskTypeInput
         };
+        // send created task data object as an argumment to createTaskEl
+        createTaskEl(taskDataObj);
     };
     //clears content of previously created task
     formEl.reset();
-
-    // send created task data object as an argumment to createTaskEl
-    createTaskEl(taskDataObj);
 };
 
 var createTaskEl = function (taskDataObj) {
@@ -80,21 +79,21 @@ var createTaskActions = function (taskId) {
     editButtonEl.setAttribute("data-task-id", taskId);
 
     actionContainerEl.appendChild(editButtonEl);
-    
+
     // create delete button
     var deleteButtonEl = document.createElement("button");
     deleteButtonEl.textContent = "Delete";
     deleteButtonEl.className = "btn delete-btn";
     deleteButtonEl.setAttribute("data-task-id", taskId);
-    
+
     actionContainerEl.appendChild(deleteButtonEl);
-    
+
     // create status dropdown
     var statusSelectEl = document.createElement("select");
     statusSelectEl.className = "select-status";
     statusSelectEl.setAttribute("name", "status-change");
     statusSelectEl.setAttribute("data-task-id", taskId);
-    
+
     // add the options to dropdown
     var statusChoices = ["To Do", "In Progress", "Completed"];
     for (var i = 0; i < statusChoices.length; i++) {
@@ -102,11 +101,11 @@ var createTaskActions = function (taskId) {
         var statusOptionEl = document.createElement("option");
         statusOptionEl.textContent = statusChoices[i];
         statusOptionEl.setAttribute("value", statusChoices[i]);
-        
+
         // append to select
         statusSelectEl.appendChild(statusOptionEl);
     };
-    
+
     actionContainerEl.appendChild(statusSelectEl);
     return actionContainerEl;
 };
@@ -148,26 +147,41 @@ var taskButtonHandler = function (event) {
 };
 
 // task status change
-var taskStatusChangeHandler = function (event){
-console.log(event.target);
-console.log(event.target.getAttribute("data-task-id"));
+var taskStatusChangeHandler = function (event) {
+//   get the task item's id
+var taskId = event.target.getAttribute("data-task-id");
+// get the currently selected option's value and convert to lowercase
+var statusValue = event.target.value.toLowerCase();
+
+// find the parent task item element based on the id
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+if(statusValue === "to do"){
+    tasksToDoEl.appendChild(taskSelected);
+}
+else if(statusValue === "in progress"){
+    tasksInProgressEl.appendChild(taskSelected);
+}
+else if(statusValue === "completed"){
+    tasksCompletedEl.appendChild(taskSelected);
+}
+ 
 };
 
 
 var editTask = function (taskId) {
     // get task list item element
     var taskSelected = document.querySelector((".task-item[data-task-id='" + taskId + "']"));
-    
+
     // get content from the task name and type
     var taskName = taskSelected.querySelector("h3.task-name").textContent;
-    
+
     var taskType = taskSelected.querySelector("span.task-type").textContent;
-    
+
     document.querySelector("input[name='task-name']").value = taskName;
     document.querySelector("select[name='task-type']").value = taskType;
     document.querySelector("#save-task").textContent = "Save Task";
     formEl.setAttribute("data-task-id", taskId);
-    
+
 };
 
 // function to delete task list item
